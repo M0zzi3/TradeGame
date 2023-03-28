7765a4907af7d24dce90d9fca0370cba4ea93556
Sprzedawcas <bartek.polawski.2015@gmail.com>
2023-03-28 03:34:47 +0200

    delet function from home

--- static/script/home.js
+++ static/script/home.js
@@ -1,20 +1,3 @@
-// window.onload = function () {
-//     const divv = document.getElementById("content");
-//     divv.innerHTML = `
-//     <div class="UserInput-Box">
-//         <div class="JoinGame-Box">
-//             <button class="start button" type="button" onclick="joinGame()">
-//                 JOIN GAME
-//             </button>
-//         </div>
-//         <div class="HostGame-Box">
-//             <button class="start button" type="button" onclick="hostGame()">
-//                 HOST GAME
-//             </button>
-//         </div>
-//     </div>`;
-// };
-
 function hostGame() {
   window.location.replace("/host");
 }
