3004741acbbad5bcceabebcd64bf72acb8f7bac0
Sprzedawcas <bartek.polawski.2015@gmail.com>
2023-03-28 03:33:33 +0200

    new home page look

--- static/script/home.js
+++ static/script/home.js
@@ -1,27 +1,27 @@
-window.onload = function () {
-    const divv = document.getElementById("content");
-    divv.innerHTML = `
-    <div class="UserInput-Box">
-        <div class="JoinGame-Box">
-            <button class="start button" type="button" onclick="joinGame()">
-                JOIN GAME
-            </button>
-        </div>
-        <div class="HostGame-Box">
-            <button class="start button" type="button" onclick="hostGame()">
-                HOST GAME
-            </button>
-        </div>
-    </div>`;
-};
+// window.onload = function () {
+//     const divv = document.getElementById("content");
+//     divv.innerHTML = `
+//     <div class="UserInput-Box">
+//         <div class="JoinGame-Box">
+//             <button class="start button" type="button" onclick="joinGame()">
+//                 JOIN GAME
+//             </button>
+//         </div>
+//         <div class="HostGame-Box">
+//             <button class="start button" type="button" onclick="hostGame()">
+//                 HOST GAME
+//             </button>
+//         </div>
+//     </div>`;
+// };
 
 function hostGame() {
-    window.location.replace("/host");
+  window.location.replace("/host");
 }
 
 function joinGame() {
-    const divv = document.getElementById("content");
-    divv.innerHTML = `<form method="post" class="buttons">
+  const divv = document.getElementById("content");
+  divv.innerHTML = `<form method="post" class="buttons">
     <label>Name:</label
     ><input type="text" placeholder="Pick a name!" name="name" value="" /><input
         type="text"
