cca11c0a337c35f2b30b8b2cd106b3e10b1989f6
Sprzedawcas <bartek.polawski.2015@gmail.com>
2023-04-05 02:28:25 +0200

    edit user game page and add Potenial Value

--- static/script/user.js
+++ static/script/user.js
@@ -1,28 +1,24 @@
 var socketio = io();
 let goldValue = [];
+walletGold = 0;
 socketio.on("userStartGame", (data) => {
-  const divLobby = document.getElementById("lobby");
-  divLobby.innerHTML = "";
-  const divGame = document.getElementById("game");
-  divGame.innerHTML = `
-        <h1>GAME</h1>
-        <h2> Money: </h2>
-        <div id="money"></div>
-        <h2> Wallet </h2>
-        <div id="wallet">Pusto Brachu</div>
-        <h2> Gold: </h2> 
-        <div id="goldvalue"></div>
-        <button class="buy" onclick="buyGold()">Buy Gold</button>
-        `;
-  document.getElementById("money").innerHTML = data.moneyOnStart;
+  console.log("stargGame");
+  document.getElementById("lobby").style.display = "none";
+  document.getElementById("game").style.display = "block";
+  document.getElementById("money").innerHTML = data.moneyOnStart + "$";
 });
 socketio.on("updateWallet", (data) => {
-  document.getElementById("money").innerHTML = data.money;
-  document.getElementById("wallet").innerHTML = data.wallet.gold;
-  // console.log();
+  document.getElementById("money").innerHTML = data.money + "$";
+  document.getElementById("wallet-gold").innerHTML = data.wallet.gold;
+  walletGold = data.wallet.gold;
+  updatePotentialGoldValue();
 });
 function buyGold() {
   socketio.emit("buy", { whatBuy: "Gold" });
+}
+function updatePotentialGoldValue() {
+  document.getElementById("potential-gold-value").innerHTML =
+    walletGold * goldValue[goldValue.length - 1] + "$";
 }
 socketio.on("updateValue", (data) => {
   goldValue.push(data.gold);
@@ -30,6 +26,6 @@ socketio.on("updateValue", (data) => {
     goldValue.shift();
   }
   console.log(goldValue);
-  console.log(data.money);
-  document.getElementById("goldvalue").innerHTML = data.gold;
+  document.getElementById("gold-value").innerHTML = data.gold;
+  updatePotentialGoldValue();
 });
